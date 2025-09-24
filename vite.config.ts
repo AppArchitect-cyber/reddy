6726fa4467edd0dbd424f1bdb6fab4794492c0fa
AppArchitect-cyber <gobinddash121@gmail.com>
2025-09-25 01:30:49 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist", // <== Ensure this is set
+  },
+  // 👇 This enables fallback to index.html for all routes like /admin
+  preview: {
+    port: 4173,
+    host: true,
+  },
+}));
